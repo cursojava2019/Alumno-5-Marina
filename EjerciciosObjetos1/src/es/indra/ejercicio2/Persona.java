ba8baca8ed6de235d5e4a49b917237fbab6e5e71
Cursojava <Cursojava@192.168.1.16>
2019-01-10 17:49:20 +0100

    Subida ejercicios

---
+++ EjerciciosObjetos1/src/es/indra/ejercicio2/Persona.java
@@ -0,0 +1,23 @@
+package es.indra.ejercicio2;
+
+public class Persona {
+	protected String nombre;
+	protected Integer edad;
+	protected String dni;
+	protected char sexo;
+	protected Float peso;
+	protected Float altura;
+
+	public Persona(String nombre, Integer edad, String dni, char sexo, Float peso, Float altura) {
+		this.nombre = "";
+		this.edad = 0;
+		this.dni = "";
+		this.sexo = "";
+		this.peso = Float.valueOf(0);
+		this.altura = Float.valueOf(0);
+
+//Métodos set de todos los constructores 
+
+	}
+
+}
